739b7f19f37e12c748a5b9a57c69997837b75083
Petter Österling <petter.osterling@aftonbladet.se>
2014-01-09 17:23:09 +0100

    Added fotbollsappstuff

--- cmd/startAppiumAndroid.sh
+++ cmd/startAppiumAndroid.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
 . ../etc/RMTest.conf
-#export APK_PATH=$1
-#if [ -z "$APK_PATH" ]; then
-#	echo "usage: Full or relative path to the apk to installed should be supplied as an argument"
-#	exit 1
-#fi
+./killAppiums.sh
+export APK_PATH=$1
+if [ -z "$APK_PATH" ]; then
+	echo "usage: Full or relative path to the apk to installed should be supplied as an argument"
+	exit 1
+fi
 
 export jar_home="$testHome/lib/selenium/"
 export androidNodeFile="/tmp/androidNodes.cfg"
@@ -24,7 +25,7 @@ do
 	cp -f $testHome/etc/Appium_TEMPLATE.json	$testHome/etc/Appium_TEMP.json
 	
 	sed -i '' "s/PLATFORM/MAC/g" $testHome/etc/Appium_TEMP.json
-#	sed -i '' "s/APP_NAME/$APK_PATH/g" $testHome/etc/Appium_TEMP.json
+	sed -i '' "s:APP_PATH:$APK_PATH:g" $testHome/etc/Appium_TEMP.json
 	sed -i '' "s/DEVICE_NAME/$modelName/g" $testHome/etc/Appium_TEMP.json
 	sed -i '' "s/DEVICE_VERSION/$androidVersion/g" $testHome/etc/Appium_TEMP.json
 	sed -i '' "s/MAX_SESSIONS/1/g" $testHome/etc/Appium_TEMP.json
@@ -32,27 +33,9 @@ do
 	sed -i '' "s/APPIUM_HOST/$RMTestLocalNodeIp/g" $testHome/etc/Appium_TEMP.json
 	sed -i '' "s/HUB_PORT/4444/g" $testHome/etc/Appium_TEMP.json
 	sed -i '' "s/HUB_HOST/$RMTestHubIp/g" $testHome/etc/Appium_TEMP.json
-#	cat $testHome/etc/Appium_TEMP.json	
+	cat $testHome/etc/Appium_TEMP.json	
 	appium -U $currDevId -a $RMTestLocalNodeIp -p $basePort --full-reset --nodeconfig $testHome/etc/Appium_TEMP.json &
 #	isInstalled=`adb -s $currDevId shell pm list packages  org.openqa.selenium.android.app`
-#    if [[ -z $isInstalled ]]
-#    then
-#	   adb -s $currDevId install -r $testHome/lib/android-server-2.32.0.apk 2>&1
-#	fi
-#	isInstalled=""
-#	if [[ $androidVersion == 2.* ]]
-#	then
-#		adb -s $currDevId shell am start -a android.intent.action.MAIN -n org.openqa.selenium.android.app/.MainActivity -e debug true 2>&1
-#	else
-#		adb -s $currDevId shell am start -S -a android.intent.action.MAIN -n org.openqa.selenium.android.app/.MainActivity -e debug true 2>&1
-#	fi
-#	
-#	sleep 1
 
-#	adb -s $currDevId forward tcp:$basePort tcp:8080 
-#	sleep 1
-#	ssh -nNT -L $RMTestLocalNodeIp:$basePort:localhost:$basePort localhost &
-#	echo "${basePort}:${currDevId}:${modelName}:${androidVersion}" >> $androidNodeFile
-#	$testHome/cmd/registerNodes.sh -hub $RMTestHubIp -node $RMTestLocalNodeIp
 done
 
