b34c546e9b79f580a50dc29c43986cbd53bf65ee
petter österling <petter@petdev.local>
2014-01-14 19:47:54 +0100

    Mac preps for demo

--- mac/startMacNode.sh
+++ mac/startMacNode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar $testHome/lib/selenium/selenium-server-standalone-2.35.0.jar -role node -nodeConfig $testHome/mac/macNodeConf.json
+java -jar $testHome/lib/selenium/selenium-server-standalone-2.39.0.jar -role node -nodeConfig $testHome/mac/macNodeConf.json
