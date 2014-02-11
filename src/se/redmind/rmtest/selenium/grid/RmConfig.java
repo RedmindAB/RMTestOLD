6549e9f0f895d2c1f9fa7615d78e2e685cc3ccec
petost <petter.osterling@gmail.com>
2014-02-11 19:01:29 +0100

    Better testhomehandling

---
+++ src/se/redmind/rmtest/selenium/grid/RmConfig.java
@@ -0,0 +1,70 @@
+package se.redmind.rmtest.selenium.grid;
+
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+public class RmConfig {
+
+static 	String configFile = TestHome.main() + "/etc/LocalConfig.json";
+
+static JSONObject config;
+	
+    public  RmConfig() {
+    	InputStream fis = null;
+		try {
+			fis = new FileInputStream(configFile);
+	
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis)); 
+        StringBuilder s = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            s.append(line);
+          }
+        br.close();
+        
+        config = new JSONObject(s.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 
+    }
+
+    public static String getConfigValue(String ConfigKey) {
+    	String configValue = null;
+    	System.out.println(config.toString());
+		try {
+			configValue = config.getJSONObject("configuration").getString(ConfigKey);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return configValue;
+    }
+    
+	public static String getHubIp() {
+		return getConfigValue("hubIp");
+	}
+	
+	public static String getLocalIp() {
+		return getConfigValue("localIp");
+	}
+ 
+    }
---
+++ src/se/redmind/rmtest/selenium/grid/TestHome.java
@@ -0,0 +1,63 @@
+package se.redmind.rmtest.selenium.grid;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TestHome {
+
+	/**
+	 * @param args
+	 */
+	 public static String main() {
+		// TODO Auto-generated method stub
+		 String testHome = null;
+		 if (isWindows()) {
+			 testHome = System.getenv("TESTHOME");
+		 } else {
+			 InputStream fis;
+			 try {
+				 fis = new FileInputStream(System.getenv("HOME") + "/.RmTest");
+
+				 BufferedReader br = new BufferedReader(new InputStreamReader(fis)); 
+				 String line;
+				 
+				 while ((line = br.readLine()) != null) {                
+					 if (line.contains("TESTHOME=")) {
+						 System.out.println(line);
+						 testHome = line.split("=")[1];
+						 System.out.println(testHome);
+					 }
+				 }
+			 } catch (FileNotFoundException e) {
+				 // TODO Auto-generated catch block
+				 e.printStackTrace();
+			 } catch (IOException e) {
+				 // TODO Auto-generated catch block
+				 e.printStackTrace();
+			 }
+		 }
+		return testHome;
+
+	 }
+
+
+
+
+	 public static String getOsName()
+	 {
+		 String OS = null;
+
+		 OS = System.getProperty("os.name");
+		 return OS;
+	 }
+
+	 public static boolean isWindows()
+	 {
+		 return getOsName().startsWith("Windows");
+	 }
+
+}
