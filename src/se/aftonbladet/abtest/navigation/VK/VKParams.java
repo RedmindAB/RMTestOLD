883692948cc6c024fea1675430b1faff462ea97e
ninham <nina.hammar@aftonbladet.se>
2014-03-21 10:40:24 +0100

    Adding lost VK files

---
+++ src/se/aftonbladet/abtest/navigation/VK/VKParams.java
@@ -0,0 +1,11 @@
+package se.aftonbladet.abtest.navigation.VK;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ninham1
+ * Date: 2014-03-21
+ * Time: 10:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class VKParams {
+}
---
+++ src/se/aftonbladet/abtest/tests/VK/VKTestRunner.java
@@ -0,0 +1,11 @@
+package se.aftonbladet.abtest.tests.VK;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ninham1
+ * Date: 2014-03-21
+ * Time: 10:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class VKTestRunner {
+}
