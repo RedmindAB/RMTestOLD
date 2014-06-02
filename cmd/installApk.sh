f533f67346331093f3a3a36508197a46d5493d5f
Petter Österling <petter.osterling@aftonbladet.se>
2014-06-02 10:56:02 +0200

    Fix for apk files with spaces in name

--- cmd/installApk.sh
+++ cmd/installApk.sh
@@ -7,7 +7,7 @@ if [ -z "$APK_PATH" ]; then
 fi
 echo "Apk to install: $APK_PATH"
 echo "adb library used: $ADB_PATH"
-export APK_PACKAGE=`$ADB_PATH/../build-tools/android-$AndroidBuildToolVersion/aapt dump badging $APK_PATH | grep package: | cut -d " " -f 2 | cut -d "'" -f 2`
+export APK_PACKAGE=`$ADB_PATH/../build-tools/android-$AndroidBuildToolVersion/aapt dump badging "$APK_PATH" | grep package: | cut -d " " -f 2 | cut -d "'" -f 2`
 export modelName=""
 
 adb devices | grep "	device" | cut -d "	" -f1 | while read currDevId
@@ -19,7 +19,7 @@ do
 	echo "Uninstalling package....."
 	$ADB_PATH/adb -s $currDevId uninstall $APK_PACKAGE 2>&1
 	echo "Installing package....."
-	$ADB_PATH/adb -s $currDevId install $APK_PATH
+	$ADB_PATH/adb -s $currDevId install "$APK_PATH"
 done
 
 
