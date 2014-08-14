188a44fdcae87ffcc64d6f982356ff4e02b99bf5
Petter Österling <petter.osterling@aftonbladet.se>
2014-08-14 14:18:19 +0200

    Cleanup

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.BeforeClass;
 
-import se.aftonbladet.abtest.tests.app.TestFotbollsAppExample;
 //import se.aftonbladet.abtest.tests.mobile.menu.AbMobileNavTest;
 import se.redmind.rmtest.selenium.grid.DriverProvider;
 import se.redmind.rmtest.selenium.tests.mobile.GoogleExampleAndroid;
