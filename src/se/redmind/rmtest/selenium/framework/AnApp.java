32e5827bb5b50d97c6afe0f363144350fd224009
Petter Österling <petter.osterling@aftonbladet.se>
2014-08-28 15:49:03 +0200

    Changed app driver

--- src/se/redmind/rmtest/selenium/framework/AnApp.java
+++ src/se/redmind/rmtest/selenium/framework/AnApp.java
@@ -1,12 +1,18 @@
 package se.redmind.rmtest.selenium.framework;
 
+import io.appium.java_client.AppiumDriver;
+
 import org.openqa.selenium.WebDriver;
 
 public class AnApp extends HTMLPage{
-	
-	public AnApp(WebDriver pDriver) {
+	private AppiumDriver appDriver;
+	public AnApp(AppiumDriver pDriver) {
 		super(pDriver);
+		appDriver = pDriver;
 		// TODO Auto-generated constructor stub
 	}
 	
+	 public final AppiumDriver getDriver() {
+	        return appDriver;
+	    }    
 }
