df01b7ba5d94d22d1598b66ae3b4a5279cda49fb
adriankabala <adrian.kabala@schibsted.pl>
2014-10-14 10:33:18 +0200

    Appium update

--- cmd/killAppiums.sh
+++ cmd/killAppiums.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 #Cleans up portforwards
-ps -e | grep "ium/bin/appium.js" | grep -v grep | sed -e 's/^ *//g' | tr -s ' ' | cut -d " " -f1 | while read thePid
+ps -e | grep "/usr/local/bin/appium" | grep -v grep | sed -e 's/^ *//g' | tr -s ' ' | cut -d " " -f1 | while read thePid
 	do
 	echo "killing $thePid"
 	kill $thePid
 done
+ps -ef | grep ios_webkit_debug_proxy | awk '{ print $2 }' | while read thePid
+do
+	echo "killing $thePid"
+	kill $thePid
+	done
