e2b96babe1794f7ad56ac51dff6c7d41bd37045d
Petter Österling <petter.osterling@aftonbladet.se>
2014-10-15 14:44:24 +0200

    Fixed false pid match

--- cmd/killAppiums.sh
+++ cmd/killAppiums.sh
@@ -7,7 +7,7 @@ ps -e | grep "bin/appium" | grep -v grep | sed -e 's/^ *//g' | tr -s ' ' | cut -
 	kill $thePid
 done
 echo "Kill ios_webkit"
-ps -ef | grep ios_webkit_debug_proxy | awk '{ print $2 }' | while read thePid
+ps -ef | grep ios_webkit_debug_proxy | grep -v grep | awk '{ print $2 }' | while read thePid
 do
 	echo "killing $thePid"
 	kill $thePid
