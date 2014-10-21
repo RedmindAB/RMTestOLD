bf84524a8350c0a0f73724ae2d0a4263cb4d45ed
Petter Österling <petter.osterling@redmind.se>
2014-10-22 00:32:27 +0200

    Changing to latest standalone server

--- windows/StartStandaloneServer.bat
+++ windows/StartStandaloneServer.bat
@@ -1,2 +1,2 @@
 @ECHO off
-java -jar selenium-server-standalone-2.35.0.jar -role node -nodeConfig Win7Wmware.json
+java -jar ../lib/selenium-server-standalone-2.43.1.jar -role node -nodeConfig Win.json
