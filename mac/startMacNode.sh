9b88e27e10568eb4c4b8ed1d9193a4ce066c2da6
Petter Österling <petter.osterling@aftonbladet.se>
2015-02-12 13:09:20 +0100

    Adding logging to script

--- mac/startMacNode.sh
+++ mac/startMacNode.sh
@@ -2,12 +2,14 @@
 
 . $testHome/cmd/setConfig.sh
 
+logName=`getLogPrefix`
+
 export NodeConfig=$1
 if [ -z "$NodeConfig" ]; then
     echo "usage: Full or relative path to a nodeconfig json file"
     exit 1
 fi
 
-java -jar $testHome/lib/selenium-server-standalone-$seleniumVersion.jar -role node -nodeConfig $NodeConfig
+java -jar $testHome/lib/selenium-server-standalone-$seleniumVersion.jar -role node -nodeConfig $NodeConfig &> $logName.log &
 
 
