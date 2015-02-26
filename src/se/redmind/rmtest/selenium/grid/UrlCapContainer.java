7d88bac9751a04fbbe35f06f1dfc8df86e6f29c9
Petter Österling <petter.osterling@aftonbladet.se>
2015-02-26 15:03:19 +0100

    Now disqualifies a driver for rest of class after 5 attempts

--- src/se/redmind/rmtest/selenium/grid/UrlCapContainer.java
+++ src/se/redmind/rmtest/selenium/grid/UrlCapContainer.java
@@ -1,8 +1,6 @@
 package se.redmind.rmtest.selenium.grid;
 
 import static org.junit.Assert.assertTrue;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 
@@ -20,13 +18,14 @@ public class UrlCapContainer {
 	private DesiredCapabilities capability;
 	String description;
 	WebDriver driver;
+	boolean imAFailure;
 
 
 	public UrlCapContainer(URL url, DesiredCapabilities capability, String description) {
 		this.url = url;
 		this.capability = capability;
 		this.description = description;
-
+		this.imAFailure = false;
 	}
 
 	public String getDescription() {
@@ -44,60 +43,67 @@ public class UrlCapContainer {
 	public WebDriver getDriver() {
 		return this.driver;
 	}
-	
+
 
 	/**
 	 * @param filteredUrlCapList
 	 */
 	public WebDriver startDriver(){
 		if (this.driver == null) {
+
+			int maxRetryAttempts = 5;
+
+			if (this.imAFailure) {
+				assertTrue("Since driver didn't start after  " + maxRetryAttempts + " attempts, it probably wont start now ",false);
+				return this.driver;
+			} else {
+				
+			int retryAttempts = 1;
 			
-		
-		int maxRetryAttempts = 5;
-		int retryAttempts = 1;
-		while (retryAttempts <= maxRetryAttempts) {
-			try {
+			while (retryAttempts <= maxRetryAttempts) {
+				try {
 
-//				WebDriver driver;
-				if (capability.getCapability("rmDeviceType") == null) {
-					this.driver = new RemoteWebDriver(url, capability);
-					System.out.println("This is a RemoteWebDriver");
-				} else {
-
-					if ("Android".equalsIgnoreCase((String) capability.getCapability("platformName")) ) {
-						this.driver = new AndroidDriver(url, capability);
+					if (capability.getCapability("rmDeviceType") == null) {
+						this.driver = new RemoteWebDriver(url, capability);
+						System.out.println("This is a RemoteWebDriver");
 					} else {
-						this.driver = new IOSDriver(url, capability);
+
+						if ("Android".equalsIgnoreCase((String) capability.getCapability("platformName")) ) {
+							this.driver = new AndroidDriver(url, capability);
+						} else {
+							this.driver = new IOSDriver(url, capability);
+						}
+
+						System.out.println("This is a AppiumDriver");
 					}
 
-					System.out.println("This is a AppiumDriver");
+					System.out.println("Started driver: " + description);
+
+				} catch (UnreachableBrowserException e) {
+					System.out.println("Having trouble starting webdriver for device: " + this.description);
+					System.out.println("Attempt " + retryAttempts + " of " + maxRetryAttempts);
+					e.printStackTrace();
+					retryAttempts++;
+					continue;
+				} catch (SessionNotCreatedException e) {
+					System.out.println("Having trouble starting webdriver for device: " + this.description);
+					System.out.println("Attempt " + retryAttempts + " of " + maxRetryAttempts);
+					e.printStackTrace();
+					retryAttempts++;
+					continue;
+				} catch (Exception e) {
+					System.out.println("Having trouble starting webdriver for device: " + this.description);
+					System.out.println("Attempt " + retryAttempts + " of " + maxRetryAttempts);
+					e.printStackTrace();
+					retryAttempts++;
+					continue;
 				}
-
-				System.out.println("Started driver: " + description);
-
-			} catch (UnreachableBrowserException e) {
-				System.out.println("Having trouble starting webdriver for device: " + this.description);
-				System.out.println("Attempt " + retryAttempts + " of " + maxRetryAttempts);
-				e.printStackTrace();
-				retryAttempts++;
-				continue;
-			} catch (SessionNotCreatedException e) {
-				System.out.println("Having trouble starting webdriver for device: " + this.description);
-				System.out.println("Attempt " + retryAttempts + " of " + maxRetryAttempts);
-				e.printStackTrace();
-				retryAttempts++;
-				continue;
-			} catch (Exception e) {
-				System.out.println("Having trouble starting webdriver for device: " + this.description);
-				System.out.println("Attempt " + retryAttempts + " of " + maxRetryAttempts);
-				e.printStackTrace();
-				retryAttempts++;
-				continue;
+				return this.driver;
 			}
+			this.imAFailure=true;
+			assertTrue("Driver failed to start properly after " + (retryAttempts - 1) + " attempts",false);
 			return this.driver;
-		}
-		assertTrue("Driver failed to start properly after " + (retryAttempts - 1) + " attempts",false);
-		return this.driver;
+			}
 		} else {
 			return this.driver;
 		}
