0b8da83c0a8db9acc2d4071a302691f079b75719
petost <petter.osterling@gmail.com>
2015-03-12 09:23:14 +0100

    Removing old legacy crap.

--- src/se/redmind/rmtest/selenium/grid/GridConstatants.java
+++ src/se/redmind/rmtest/selenium/grid/GridConstatants.java
@@ -2,11 +2,5 @@ package se.redmind.rmtest.selenium.grid;
 
 
 public class GridConstatants {
-    //File where android nodes are stored by startWebDriverOnAndroids.sh in tmp directory
-    public static String androidNodes = "/tmp/androidNodes.cfg";
-//    public static String hubHost = "10.12.11.225";          //10.12.11.225
-//    public static String hubHost = "10.12.3.149";          //10.12.11.225
-//  public static String hubHost = "localhost";          //10.12.11.225
     public static int hubPort = 4444;
-    
 }
