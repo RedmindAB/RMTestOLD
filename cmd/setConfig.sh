2909168102d1af91f915a9dd143db86940f5f054
petost <petter.osterling@gmail.com>
2015-03-16 23:39:06 +0100

    Creating a new config file, RmConfig.json This might make it easier to push selenium updates.

--- cmd/setConfig.sh
+++ cmd/setConfig.sh
@@ -2,21 +2,24 @@
 
 export testHome=`grep TESTHOME ~/.RmTest | cut -d "=" -f2`
 
-getConfigParam() {
+getLocalConfigParam() {
 	$testHome/cmd/json.sh -l < $testHome/etc/LocalConfig.json | grep "\"configuration\",\"$1\"" | cut -d"]" -f2 | tr -d '\t' | sed 's/"//g'
 }
+getRmConfigParam() {
+	$testHome/cmd/json.sh -l < $testHome/etc/RmConfig.json | grep "\"configuration\",\"$1\"" | cut -d"]" -f2 | tr -d '\t' | sed 's/"//g'
+}
 
-export androidHome=`getConfigParam androidHome`
+export androidHome=`getLocalConfigParam androidHome`
 export ANDROIDHOME=$androidHome
 export ANDROID_HOME=$androidHome
 export ADB_PATH="$ANDROIDHOME/platform-tools"
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export RmJar="$testHome/target/RMTest-SNAPSHOT.jar"
-export RMTestHubIp=`getConfigParam hubIp`
-export RMTestLocalNodeIp=`getConfigParam localIp`
-#export AndroidBuildToolVersion=`getConfigParam AndroidBuildtoolsVersion`
+export RMTestHubIp=`getLocalConfigParam hubIp`
+export RMTestLocalNodeIp=`getLocalConfigParam localIp`
+#export AndroidBuildToolVersion=`getLocalConfigParam AndroidBuildtoolsVersion`
 export AAPTCmd=`find $androidHome -name aapt | head -1`
-export seleniumVersion=`getConfigParam seleniumVersion`
+export seleniumVersion=`getRmConfigParam seleniumVersion`
 export PRETTY_TIMESTAMP="date +%Y%m%d_%H%M%S"
 
 
