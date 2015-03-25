12ec47fc664c7193abb275a4bdfbaa68a0bc99ad
petost <petter.osterling@gmail.com>
2015-03-25 18:36:54 +0100

    Now uses ADB_PATH

--- cmd/showDeviceInfo.sh
+++ cmd/showDeviceInfo.sh
@@ -6,10 +6,10 @@ export modelName=""
 export androidVersion=""
 
 echo "Connected devices:"
-adb devices | grep "	device" | cut -d "	" -f1 | while read currDevId
+$ADB_PATH/adb devices | grep "	device" | cut -d "	" -f1 | while read currDevId
 do
-	modelName=`adb -s $currDevId shell getprop ro.product.model | tr -d "\r"`
-	androidVersion=`adb -s $currDevId shell getprop ro.build.version.release | tr -d "\r"`
+	modelName=`$ADB_PATH/adb -s $currDevId shell getprop ro.product.model | tr -d "\r"`
+	androidVersion=`$ADB_PATH/adb -s $currDevId shell getprop ro.build.version.release | tr -d "\r"`
 	echo "----------------------------------------------------"
 	echo "Model: $modelName ---- AndroidVersion: $androidVersion ---- ID: $currDevId"
 
