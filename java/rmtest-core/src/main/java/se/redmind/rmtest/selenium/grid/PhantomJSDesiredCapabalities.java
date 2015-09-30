e08395c405ee5aa134bf519f6c017e69e11f937b
johgri <johan.grimlund@aftonbladet.se>
2015-09-30 12:18:25 +0200

    Added some capabilities and deleted some

--- java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSDesiredCapabalities.java
+++ java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSDesiredCapabalities.java
@@ -10,22 +10,14 @@ import java.util.Map;
 /**
  * Created by johgri on 15-09-29.
  */
-public class PhantomJSDesiredCapabalities{
-
-    public Capabilities createPhantomJSCapabilities(){
+public class PhantomJSDesiredCapabalities {
+    public Capabilities createPhantomJSCapabilities() {
         DesiredCapabilities caps = new DesiredCapabilities();
         ArrayList<String> cliArgsCap = new ArrayList<String>();
-
         caps.setCapability("phantomjs.binary.path", "/usr/local/bin/phantomjs");
-        cliArgsCap.add("--web-security=false");
-        cliArgsCap.add("--ssl-protocol=any");
-        cliArgsCap.add("--ignore-ssl-errors=true");
         caps.setCapability("takesScreenshot", true);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--remote-debugger-port=9000"} );
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--remote-debugger-port=9000", "--remote-debugger-autorun=yes", "--logLevel=2"});
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--logLevel=DEBUG"});
-
         return caps;
     }
-
 }
