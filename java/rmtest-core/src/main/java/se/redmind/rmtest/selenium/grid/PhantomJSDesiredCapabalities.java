704c4691139dedf9424cc820b5819abb52182846
johgri <johan.grimlund@aftonbladet.se>
2015-10-06 12:45:03 +0200

    Changed some capabilities for phantomJS and changed GridConstatants

--- java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSDesiredCapabalities.java
+++ java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSDesiredCapabalities.java
@@ -14,9 +14,10 @@ public class PhantomJSDesiredCapabalities {
     public Capabilities createPhantomJSCapabilities() {
         DesiredCapabilities caps = new DesiredCapabilities();
         ArrayList<String> cliArgsCap = new ArrayList<String>();
-        caps.setCapability("phantomjs.binary.path", "/usr/local/bin/phantomjs");
+        cliArgsCap.add("--webdriver=6666");
         caps.setCapability("takesScreenshot", true);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--remote-debugger-port=9000", "--remote-debugger-autorun=yes", "--logLevel=2"});
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--remote-debugger-port=6666"+"/Users/johgri/Documents/dev/RMTest/java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSScripts/TestDebugging.js", "--remote-debugger-autorun=yes", "--logLevel=5"});
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/local/bin/phantomjs");
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
         return caps;
     }
