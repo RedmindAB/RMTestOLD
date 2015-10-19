fa738aa11d0b9ba423a4c730b0fd67bea377caf7
johgri <johan.grimlund@aftonbladet.se>
2015-10-19 16:57:20 +0200

    Changes to DesiredCapabilities to ignore ssl errors in https requests

--- java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSDesiredCapabalities.java
+++ java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSDesiredCapabalities.java
@@ -10,18 +10,17 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  * Created by johgri on 15-09-29.
  */
 public class PhantomJSDesiredCapabalities {
-
     public Capabilities createPhantomJSCapabilities() {
         DesiredCapabilities caps = new DesiredCapabilities();
-        ArrayList<String> cliArgsCap = new ArrayList<>();
+        ArrayList<String> cliArgsCap = new ArrayList<String>();
         cliArgsCap.add("--webdriver=6666");
+        cliArgsCap.add("--ignore-ssl-errors=true");
+        cliArgsCap.add("--ssl-protocol=any");
+        cliArgsCap.add("--remote-debugger-autorun=yes");
+        cliArgsCap.add("--remote-debugger-port=6666");
         caps.setCapability("takesScreenshot", true);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
-            new String[]{
-                "--remote-debugger-port=6666" + "/Users/johgri/Documents/dev/RMTest/java/rmtest-core/src/main/java/se/redmind/rmtest/selenium/grid/PhantomJSScripts/TestDebugging.js",
-                "--remote-debugger-autorun=yes", "--logLevel=5"
-            });
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/local/bin/phantomjs");
+        caps.setCapability("phantomjs.page.settings.userAgent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 s");
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
         return caps;
     }
