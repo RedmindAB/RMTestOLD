514f74066701ce5d518de86e443b83160ec99a5e
Jeremy Comte <jeremy.comte@redmind.se>
2015-10-19 08:49:41 +0200

    extracted logback to a new module added setVersion.sh script and plugin fixed platform based detection for phantomjs on linux

--- java/rmtest-example/src/test/java/se/redmind/rmtest/selenium/example/RedmindTests.java
+++ java/rmtest-example/src/test/java/se/redmind/rmtest/selenium/example/RedmindTests.java
@@ -6,10 +6,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import se.redmind.rmtest.selenium.grid.DriverProvider;
+import se.redmind.utils.LogBackUtil;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({RMExample.class})
 public class RedmindTests {
+
+    static {
+        LogBackUtil.install();
+    }
 
     @BeforeClass
     public static void beforeAllTests() {
