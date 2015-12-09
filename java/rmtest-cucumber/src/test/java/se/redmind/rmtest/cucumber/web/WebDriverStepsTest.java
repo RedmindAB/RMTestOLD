b9c7d6b714e80244a42d3f09cba777c9eaeda669
Jeremy Comte <jeremy.comte@gmail.com>
2015-12-09 20:44:51 +0100

    removing classpath entry from rmtest-core target jar as it messes up with the exclusion rule in rmtest-cucumber

--- java/rmtest-cucumber/src/test/java/se/redmind/rmtest/cucumber/web/WebDriverStepsTest.java
+++ java/rmtest-cucumber/src/test/java/se/redmind/rmtest/cucumber/web/WebDriverStepsTest.java
@@ -1,20 +1,19 @@
 package se.redmind.rmtest.cucumber.web;
 
-import java.net.URLClassLoader;
-import java.util.Arrays;
-
 import org.junit.BeforeClass;
 import org.junit.runners.Parameterized;
 
 import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
 import se.redmind.rmtest.cucumber.CucumberParametersRunnerFactory;
+import se.redmind.rmtest.runners.DriverRunner;
 
 import static spark.Spark.*;
 
 /**
  * @author Jeremy Comte
  */
-//@RunWith(DriverRunner.class)
+@RunWith(DriverRunner.class)
 @Parameterized.UseParametersRunnerFactory(CucumberParametersRunnerFactory.class)
 @CucumberOptions(plugin = "gherkin.formatter.NonRepeatingFormatter")
 public class WebDriverStepsTest {
@@ -25,7 +24,6 @@ public class WebDriverStepsTest {
     public static synchronized void createTestServer() {
         if (!isServerRunning) {
             isServerRunning = true;
-            System.out.println(Arrays.toString(((URLClassLoader) (Thread.currentThread().getContextClassLoader())).getURLs()));
             get("/", (request, response) -> "hello!");
             get("/cookie/valueOf/:name", (request, response) -> {
                 response.type("text/plain");
